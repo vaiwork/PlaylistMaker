c3743b750363d050bec279be351091b236c55e8f
vai <vaiwork@yandex.ru>
2022-12-24 12:51:02 +0300

    Homework 3 from Sprint 8 Theme 4 Added navigation from Main activity to Search, Media and Settings activities.

--- app/src/main/java/com/vaiwork/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/vaiwork/playlistmaker/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.vaiwork.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonSearchClickListener : View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажата кнопка поиск!", Toast.LENGTH_LONG).show()
+                val searchIntent = Intent(applicationContext, SearchActivity::class.java)
+                startActivity(searchIntent)
             }
         }
         buttonSearch.setOnClickListener(buttonSearchClickListener)
@@ -23,13 +24,15 @@ class MainActivity : AppCompatActivity() {
         // Button Media lambda
         val buttonMedia = findViewById<Button>(R.id.button_media)
         buttonMedia.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажата кнопка медиа!", Toast.LENGTH_LONG).show()
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
         }
 
         // Button Settings lambda
         val buttonSettings = findViewById<Button>(R.id.button_settings)
         buttonSettings.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажата кнопка настройки!", Toast.LENGTH_LONG).show()
+            val settingsIntent = Intent(this, SetingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
