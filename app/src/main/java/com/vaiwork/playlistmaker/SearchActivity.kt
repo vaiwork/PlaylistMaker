3075a0ff4109d1d5db807710532cfb98c88365f4
vai <vaiwork@yandex.ru>
2023-01-06 20:32:24 +0300

    Sprint 9, EditText TextWatcher, HW 3. 1. Create SettingsActivity with EditText and back Toolbar. 2. Add logic for clear text action: hide input keyboard, empty edit text view. 3. Add logic for clear text image: visible if edit text not empty, gone id edit text empty

--- app/src/main/java/com/vaiwork/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/vaiwork/playlistmaker/SearchActivity.kt
@@ -1,11 +1,52 @@
 package com.vaiwork.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchEditText = findViewById<EditText>(R.id.activity_search_edit_text)
+        val clearImageView = findViewById<ImageView>(R.id.activity_search_clear_image_view)
+
+        val editViewTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearImageView.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        searchEditText.addTextChangedListener(editViewTextWatcher)
+
+        clearImageView.setOnClickListener {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            searchEditText.setText("")
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.search_toolbar)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
